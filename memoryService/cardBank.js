de3ffedff1d2d3b15d163da593698893cd01039c
Vítor Eulalio Reis <reis.vitore@gmail.com>
2020-12-06 15:04:35 +0100

    memory service added

---
+++ memoryService/cardBank.js
@@ -0,0 +1,12 @@
+cardBank = [
+  {
+    close: true,
+    framework: "angular2",
+    complete: false,
+    className:"back",
+    framework_img_url:"https://raw.githubusercontent.com/samiheikki/javascript-guessing-game/master/static/logos/angular2.png",
+    id: "099099",
+  }
+];
+
+module.exports.cardBank = cardBank;
