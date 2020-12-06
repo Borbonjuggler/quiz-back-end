de10c5782ea5e48be247d61699378bc723df6037
Vítor Eulalio Reis <reis.vitore@gmail.com>
2020-12-06 13:50:33 +0100

    adding db mock

---
+++ memoryService/memoryGame.js
@@ -0,0 +1,12 @@
+memoryGame = [  //https://gist.github.com/guitarhub786/4dbbd2209667ba174ec9a534c29c3d73
+  {
+    close: True,
+    framework: "angular2",
+    complete: False,
+    className:"back",
+    framework_img_url:"https://raw.githubusercontent.com/samiheikki/javascript-guessing-game/master/static/logos/angular2.png"
+    id: "099099"
+  },
+];
+
+module.exports.memoryGame = memoryGame;
