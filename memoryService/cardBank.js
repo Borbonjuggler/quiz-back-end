464fafea40930e5edcda052fbd3202371f4b022c
Vítor Eulalio Reis <reis.vitore@gmail.com>
2020-12-06 15:28:51 +0100

    API connected

--- memoryService/cardBank.js
+++ memoryService/cardBank.js
@@ -3,7 +3,7 @@ cardBank = [
     close: true,
     framework: "angular2",
     complete: false,
-    className:"back",
+    side:"back",
     framework_img_url:"https://raw.githubusercontent.com/samiheikki/javascript-guessing-game/master/static/logos/angular2.png",
     id: "099099",
   }
